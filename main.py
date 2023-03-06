9979e4e7d8bfd51b5a4a82597c1b836fd40bfa4c
BradenAzis <Bradenkai747@gmail.com>
2023-03-05 21:20:00 -0500

    Created the main() and encode() funtions. Functions work as intended. Option 2 of menu still needs to be implemented.

---
+++ main.py
@@ -0,0 +1,53 @@
+########################################
+#              COP3502C                #
+#               Lab 6                  #
+#           Encoder/Decoder            #
+#       Blake Owen, Braden Azis        #
+########################################
+
+# encode function that adds 3 to each digit
+def encode(password):
+    encode_p = ''
+    for i in range(len(password)):  # loops through each digit of password passed through
+        digit = int(password[i]) + 3  # adds 3 to each looped digit
+
+        if digit > 9:  # checks if digit becomes greater than 9, and subtracts 10 if true
+            digit -= 10
+
+        digit = str(digit)  # converts digit to string and adds it to existing string variable encode_p
+        encode_p += digit
+
+    return encode_p
+
+
+# main function of the program with menu
+def main():
+    program_run = True
+    while program_run:
+        print('''Menu  
+------------- 
+ 1. Encode  
+2. Decode  
+3. Quit\n''')
+
+        option = int(input('Please enter an option: '))
+
+        if option == 1:
+            password = (input('Please enter your password to encode: '))
+
+            encoded_pass = encode(password)
+
+            print('Your password has been encoded and stored!\n')
+
+        elif option == 2:
+            pass      # TODO: partner add/call decode function
+
+        elif option == 3:
+            program_run = False
+
+        else:
+            print('Error! Invalid selection!')
+
+
+if __name__ == '__main__':
+    main()
